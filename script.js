2fab3d856fa024aaba4173d5c7c16d9a93a86def
zulutkachenko <zulutkachenko@gmail.com>
2021-12-29 14:32:12 +0200

    lessons 3

--- script.js
+++ script.js
@@ -61,7 +61,6 @@ function showMyDB(hidden) {
 }
 showMyDB(personalMovieDB.privat);
 
-
 function writeYourGenres() {
     for (let i = 1; i <= 3; i++) {
         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
